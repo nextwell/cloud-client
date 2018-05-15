0b64347a2be8504d1614341a061ebd7b3a3b648d
nextwell <just.nextwell@gmail.com>
2018-05-15 19:07:24 +0300

    Ого, изменил текст ;d

--- src/components/MainFunctions.jsx
+++ src/components/MainFunctions.jsx
@@ -15,9 +15,9 @@ const props = {
       console.log(info.file, info.fileList);
     }
     if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
+      message.success(`${info.file.name} загружен успешно.`);
     } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
+      message.error(`${info.file.name} не загрузился, ошибка.`);
     }
   },
 };
